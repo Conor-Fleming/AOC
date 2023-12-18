b54396cf617218d11581afb886209a73da146d34
Conor Fleming <conorfleming03@gmail.com>
2023-12-18 12:57:06 -0800

    numbering and pairing galaxies

--- 2023/day11/part1/main.go
+++ 2023/day11/part1/main.go
@@ -4,25 +4,61 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"slices"
+	"strconv"
 )
+
+type galaxyPair struct {
+	first, second galaxy
+}
+
+type galaxy struct {
+	val  string
+	x, y int
+}
 
 func main() {
 	grid := readFile("../input.txt")
 	grid = expandUnivRows(grid)
 	grid = expandUnivCols(grid)
 
-	for _, v := range grid {
-		fmt.Println(string(v))
-	}
+	//get slice of pairs of galaxies
+	pairs := numberAndPair(grid)
+	fmt.Println(len(pairs))
 
 }
 
-func expandUnivCols(grid [][]rune) [][]rune {
+func numberAndPair(grid [][]string) map[galaxyPair]bool {
+	count := 1
+	pairs := make(map[galaxyPair]bool)
+	gals := make([]galaxy, 0)
+	for i, v := range grid {
+		for j, val := range v {
+			if val == "#" {
+				grid[i][j] = strconv.Itoa(count)
+				gals = append(gals, galaxy{grid[i][j], i, j})
+				count++
+			}
+		}
+	}
+
+	for i, v := range gals {
+		for j := i + 1; j < len(gals); j++ {
+			pair := galaxyPair{v, gals[j]}
+			if _, ok := pairs[pair]; !ok {
+				pairs[pair] = true
+			}
+		}
+	}
+
+	return pairs
+}
+
+func expandUnivCols(grid [][]string) [][]string {
 	indexes := make([]int, 0)
 	for i := 0; i < len(grid[0]); i++ {
 		for j := 0; j < len(grid); j++ {
-			if grid[j][i] == '#' {
+			if grid[j][i] == "#" {
 				break
 			}
 
@@ -36,13 +72,12 @@ func expandUnivCols(grid [][]rune) [][]rune {
 	return addCol(grid, indexes)
 }
 
-func addCol(grid [][]rune, indexes []int) [][]rune {
-	fmt.Println(indexes)
+func addCol(grid [][]string, indexes []int) [][]string {
 	for ext, v := range indexes {
 		for i := range grid {
-			firstPart := append([]rune{}, grid[i][:v+1+ext]...)
-			secondPart := append([]rune{}, grid[i][v+1+ext:]...)
-			firstPart = append(firstPart, '.')
+			firstPart := append([]string{}, grid[i][:v+1+ext]...)
+			secondPart := append([]string{}, grid[i][v+1+ext:]...)
+			firstPart = append(firstPart, ".")
 			grid[i] = append(firstPart, secondPart...)
 		}
 	}
@@ -50,11 +85,11 @@ func addCol(grid [][]rune, indexes []int) [][]rune {
 	return grid
 }
 
-func expandUnivRows(grid [][]rune) [][]rune {
+func expandUnivRows(grid [][]string) [][]string {
 	for i, v := range grid {
-		if !strings.Contains(string(v), "#") {
-			firstPart := append([][]rune{}, grid[:i+1]...)
-			secondPart := append([][]rune{}, grid[i+1:]...)
+		if !slices.Contains(v, "#") {
+			firstPart := append([][]string{}, grid[:i+1]...)
+			secondPart := append([][]string{}, grid[i+1:]...)
 			firstPart = append(firstPart, v)
 			grid = append(firstPart, secondPart...)
 		}
@@ -63,7 +98,7 @@ func expandUnivRows(grid [][]rune) [][]rune {
 	return grid
 }
 
-func readFile(filepath string) [][]rune {
+func readFile(filepath string) [][]string {
 	//read contents of file to lines array
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -78,11 +113,11 @@ func readFile(filepath string) [][]rune {
 		lines = append(lines, scanner.Text())
 	}
 
-	grid := make([][]rune, 0)
+	grid := make([][]string, 0)
 	for i, v := range lines {
-		grid = append(grid, []rune{})
+		grid = append(grid, []string{})
 		for _, val := range v {
-			grid[i] = append(grid[i], val)
+			grid[i] = append(grid[i], string(val))
 		}
 	}
 
