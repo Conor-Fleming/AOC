e6cc3a5e23f22bae4b3aa081b67cec3749719e14
Conor-Fleming <conorfleming03@gmail.com>
2024-12-01 20:40:34 -0700

    part 1

---
+++ 2024/day1/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	left := make([]int, 0)
+	right := make([]int, 0)
+	for i, v := range readFile("input.txt") {
+		v, _ := strconv.Atoi(v)
+		if i%2 == 0 {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+
+	fmt.Println(part1(left, right))
+	fmt.Println(part2(left, right))
+}
+
+func part1(left, right []int) int {
+	sort.Slice(left, func(i, j int) bool {
+		return left[i] < left[j]
+	})
+	sort.Slice(right, func(i, j int) bool {
+		return right[i] < right[j]
+	})
+
+	distances := make([]float64, 0)
+	for i, v := range left {
+		distances = append(distances, math.Abs(float64(v-right[i])))
+	}
+
+	sum := 0
+	for _, v := range distances {
+		sum += int(v)
+	}
+
+	return sum
+}
+
+func part2(left, right []int) string {
+
+	return ""
+}
+
+func readFile(filepath string) []string {
+	//read contents of file to lines array
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var words []string
+	//var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		// Split the line into words
+		lineWords := strings.Fields(scanner.Text())
+		//ines = append(lines, scanner.Text())
+
+		// Append each word to the slice
+		words = append(words, lineWords...)
+	}
+
+	return words
+}
